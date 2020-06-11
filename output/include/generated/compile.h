9c5e280af705a553b2771412cd83da1a9a20331e
T199 <tirupathitwo@gmail.com>
2020-06-11 10:33:17 +0530

    kernel-first-commit

---
+++ output/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jun 11 09:31:26 IST 2020"
+#define LINUX_COMPILE_BY "timesys"
+#define LINUX_COMPILE_HOST "timesys"
+#define LINUX_COMPILER "gcc version 6.5.0 (Timesys 20200522) "
